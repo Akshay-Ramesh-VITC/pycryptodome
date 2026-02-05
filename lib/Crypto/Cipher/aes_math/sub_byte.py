b09fd0f1d711cd88f9fb333e82482232c11d1cb6
Akshay R <157674473+Akshay-Ramesh-VITC@users.noreply.github.com>
2026-02-05 11:50:18 +0530

    Refactor import statements in sub_byte.py

--- lib/Crypto/Cipher/aes_math/sub_byte.py
+++ lib/Crypto/Cipher/aes_math/sub_byte.py
@@ -1,5 +1,6 @@
-from aes_math.gf import gf_inv
-from aes_math.affine_transformation import affine_transform
+from .gf import gf_inv
+from .affine_transformation import affine_transform
 
 def sub_byte(byte):
     return affine_transform(gf_inv(byte))
+
